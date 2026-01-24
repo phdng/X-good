9336ab361c44fd0531979c8626e67dd2528f7906
vuong4-design <inctrancvuong4@gmail.com>
2026-01-25 05:27:53 +0700

    ok

--- ProjectXTweak/AAA_TestCtor.m
+++ ProjectXTweak/AAA_TestCtor.m
@@ -1,8 +1,21 @@
-// Simplest possible constructor test - no dependencies
+// Test using +load method instead of constructor
 #import <Foundation/Foundation.h>
 
+@interface AAA_TestLoader : NSObject
+@end
+
+@implementation AAA_TestLoader
+
++ (void)load {
+    // +load is called automatically when the class is loaded
+    [@"AAA_LOAD_METHOD_RAN" writeToFile:@"/tmp/AAA_load_test.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    NSLog(@"[WeaponX-DEBUG] +load method executed!");
+}
+
+@end
+
+// Also try constructor
 __attribute__((constructor))
 static void AAA_TestCtor_init(void) {
-    // Write flag file immediately - no other code
     [@"AAA_TEST_CTOR_RAN" writeToFile:@"/tmp/AAA_ctor_test.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
